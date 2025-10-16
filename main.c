da822489eadce4fa625e5806ad1f51255987a4ab
expfunction <burakyazar2013@gmail.com>
2025-10-16 17:51:58 +0300

    feat: add initial project structure with makefile and precompiled headers

---
+++ main.c
@@ -0,0 +1,6 @@
+#include "pch/pc_.h"
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
---
+++ pch/pc_.h
@@ -0,0 +1,20 @@
+/* CVGA Precompiled header file */
+#ifndef PC_H
+#define PC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <conio.h>
+#include <dos.h>
+#include <pc.h>
+#include <keys.h>
+#include <dpmi.h>
+#include <go32.h>
+#include <time.h>
+#include <sys/movedata.h>
+#include <sys/farptr.h>
+#include <sys/nearptr.h>
+
+#endif /* PC_H */
